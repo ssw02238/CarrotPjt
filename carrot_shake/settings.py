4928753eabb2bdadb8bb93d819dcf97d839909f4
bagjo28841 <bagjo28841@gmail.com>
2021-04-21 13:59:33 +0900

    profile page modified

--- carrot_shake/settings.py
+++ carrot_shake/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'carrot_shake' / 'static',]
+STATICFILES_DIRS = [BASE_DIR / 'carrot_shake' / 'static', BASE_DIR / 'accounts' / 'static',]
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/' 
 
