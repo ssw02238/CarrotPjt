304e076ca3e618599e05b3d332ee926dd50ad631
bibibic44 <bibibic@gmail.com>
2021-04-26 20:17:23 +0900

    forms.py 위젯 추가

--- articles/forms.py
+++ articles/forms.py
@@ -3,6 +3,33 @@ from .models import Article, Comment
 
 
 class ArticleForm(forms.ModelForm):
+    title = forms.CharField(
+        label='',
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': '제목',
+                'maxlength': 10,
+            }
+        ),
+    )
+
+    price = forms.IntegerField(
+        label='',
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': '가격',
+            }
+        ),
+    )
+
+    content = forms.CharField(
+        label='',
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': '내용',
+            }
+        ),
+    )
 
     class Meta:
         model = Article
